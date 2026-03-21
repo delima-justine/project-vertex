89a6a63f68d99169777d92250275d9e907b7f969
delima-justine <delimajustine24@gmail.com>
2026-03-21 22:32:29 +0800

    feat: add initial reports page.

---
+++ frontend/esmis/src/app/reports/reports.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-reports',
+  imports: [Sidebar],
+  templateUrl: './reports.html',
+  styleUrl: './reports.scss',
+})
+export class Reports {
+
+}
