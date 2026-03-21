e8f8cfe787035c7b758659874c034b602518d64d
delima-justine <delimajustine24@gmail.com>
2026-03-21 22:29:23 +0800

    feat: add notifications page.

---
+++ frontend/esmis/src/app/notifications/notifications.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-notifications',
+  imports: [Sidebar],
+  templateUrl: './notifications.html',
+  styleUrl: './notifications.scss',
+})
+export class Notifications {
+
+}
