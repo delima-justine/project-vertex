16492bd64d9a2ae6f5dc74fb4d8767954fef39db
delima-justine <delimajustine24@gmail.com>
2026-03-22 17:18:54 +0800

    feat: add initial create-request page.

---
+++ frontend/esmis/src/app/create-request/create-request.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-create-request',
+  imports: [Sidebar],
+  templateUrl: './create-request.html',
+  styleUrl: './create-request.scss',
+})
+export class CreateRequest {
+
+}
