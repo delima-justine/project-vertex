434e323a22b0dc59cc4d19e1f9c17a464c03ddc8
delima-justine <delimajustine24@gmail.com>
2026-03-22 16:55:28 +0800

    feat: enhance orders section with dropdown options in sidebar

--- frontend/esmis/src/app/sidebar/sidebar.ts
+++ frontend/esmis/src/app/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -8,5 +8,9 @@ import { RouterLink } from "@angular/router";
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  isDropdownOpen = signal(false);
 
+  toggleDropdown() {
+    this.isDropdownOpen.set(!this.isDropdownOpen());
+  }
 }
