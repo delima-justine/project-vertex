43efb195575ef2ecfa4799bf94c85aa208df28e3
delima-justine <delimajustine24@gmail.com>
2026-03-27 14:02:31 +0800

    feat: add Disapproved component with HTML, SCSS, and tests

---
+++ frontend/esmis/src/app/disapproved/disapproved.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-disapproved',
+  imports: [Sidebar],
+  templateUrl: './disapproved.html',
+  styleUrl: './disapproved.scss',
+})
+export class Disapproved {
+
+}
