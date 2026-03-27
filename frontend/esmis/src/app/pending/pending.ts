cd32745c2636dbe3892a866b0b3235e2c4f342c4
delima-justine <delimajustine24@gmail.com>
2026-03-27 13:13:21 +0800

    feat: add pending requests page with routing and component structure

---
+++ frontend/esmis/src/app/pending/pending.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-pending',
+  imports: [Sidebar],
+  templateUrl: './pending.html',
+  styleUrl: './pending.scss',
+})
+export class Pending {
+
+}
