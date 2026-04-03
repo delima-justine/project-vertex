48db4284e2506d8a55774007e3d67082f4cb72c3
delima-justine <delimajustine24@gmail.com>
2026-04-03 19:20:39 +0800

    feat: add Help and Support component with HTML, SCSS, and unit tests

---
+++ frontend/esmis/src/app/help-and-support/help-and-support.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+
+@Component({
+  selector: 'app-help-and-support',
+  imports: [Sidebar],
+  templateUrl: './help-and-support.html',
+  styleUrl: './help-and-support.scss',
+})
+export class HelpAndSupport {
+
+}
