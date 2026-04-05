8f289ab0d267d7e726d9ce7ed0e0e27d476550ed
delima-justine <delimajustine24@gmail.com>
2026-04-06 03:53:02 +0800

    feat: add resource route for UnitController to manage units

--- backend/smis/routes/web.php
+++ backend/smis/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SupplyController;
+use App\Http\Controllers\UnitController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -12,3 +13,4 @@ Route::get('/', function () {
 Route::resource('users', UserController::class);
 Route::get('/users', [UserController::class, 'index']);
 Route::resource('supplies', SupplyController::class);
+Route::resource('units', UnitController::class);
