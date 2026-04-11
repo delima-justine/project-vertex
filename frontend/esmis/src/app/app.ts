be34b1e0b7293f5a033a37ba855cd92a8a461665
delima-justine <delimajustine24@gmail.com>
2026-04-11 21:18:00 +0800

    feat: integrate ngx-loading-bar for enhanced loading indicators

--- frontend/esmis/src/app/app.ts
+++ frontend/esmis/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxLoadingBar } from '@ngx-loading-bar/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxLoadingBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
