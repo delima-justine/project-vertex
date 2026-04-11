62dea782396562d4eb7a93c8b69eb20a4c888c67
delima-justine <delimajustine24@gmail.com>
2026-04-11 20:09:59 +0800

    feat: enhance login functionality with error handling and form validation

--- frontend/esmis/src/app/authentication/authentication.ts
+++ frontend/esmis/src/app/authentication/authentication.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-authentication',
@@ -13,10 +14,11 @@ export class Authentication {
   router = inject(Router);
   loginForm: FormGroup;
   formBuilder = inject(FormBuilder);
+  authService = inject(AuthService);
 
   constructor() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required, Validators.email],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
   }
@@ -26,7 +28,19 @@ export class Authentication {
   }
 
   login() {
-    // Initial Implementation
-    console.log('Login form submitted:', this.loginForm.value);
+    const loginValue = this.loginForm.value;
+
+    if(this.loginForm.valid) {
+      this.authService.login(loginValue).subscribe({
+        next: (res) => {
+          console.log('Login successful:', res);
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error('Login failed:', err);
+          alert("Invalid email or password. Please try again.");
+        }
+      });
+    }
   }
 }
