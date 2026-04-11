19a2e531b66188807813ee9bb6fda80c9235272b
delima-justine <delimajustine24@gmail.com>
2026-04-11 20:32:27 +0800

    feat: add route guards for authentication and guest access

--- frontend/esmis/src/app/app.routes.ts
+++ frontend/esmis/src/app/app.routes.ts
@@ -1,65 +1,79 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './guards/guest-guard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
     path: '', 
     loadComponent: () => import('./authentication/authentication')
       .then(m => m.Authentication),
-    title: 'Login to SMIS'
+    title: 'Login to SMIS',
+    canActivate: [guestGuard]
   },
   {
     path: 'home',
     loadComponent: () => import('./home/home').then(m => m.Home),
-    title: 'Dashboard'
+    title: 'Dashboard',
+    canActivate: [authGuard]
   },
   {
     path: 'admin/user-management',
     loadComponent: () => import('./user-management/user-management')
       .then(m => m.UserManagement),
-    title: 'User Management'
+    title: 'User Management',
+    canActivate: [authGuard]
+
   },
   {
     path: 'notifications',
     loadComponent: () => import('./notifications/notifications')
       .then(m => m.Notifications),
-    title: 'Notifications'
+    title: 'Notifications',
+    canActivate: [authGuard]
   }, 
   {
     path: 'reports',
     loadComponent: () => import('./reports/reports').then(m => m.Reports),
-    title: 'Reports'
+    title: 'Reports',
+    canActivate: [authGuard]
   },
   {
     path: 'create-request',
     loadComponent: () => import('./create-request/create-request')
       .then(m => m.CreateRequest),
-    title: 'Create Request'
+    title: 'Create Request',
+    canActivate: [authGuard]
   },
   {
     path: 'pending-requests',
     loadComponent: () => import('./pending/pending').then(m => m.Pending),
-    title: 'Pending Requests'
+    title: 'Pending Requests',
+    canActivate: [authGuard]
   },
   {
     path: 'approved-requests',
     loadComponent: () => import('./approved/approved').then(m => m.Approved),
-    title: 'Approved Requests'
+    title: 'Approved Requests',
+    canActivate: [authGuard]
   },
   {
     path: 'released-requests',
     loadComponent: () => import('./released/released').then(m => m.Released),
-    title: 'Released Requests'
+    title: 'Released Requests',
+    canActivate: [authGuard]
   },
   {
     path: 'disapproved-requests',
     loadComponent: () => import('./disapproved/disapproved')
       .then(m => m.Disapproved),
-    title: 'Disapproved Requests'
+    title: 'Disapproved Requests',
+    canActivate: [authGuard]
   },
   {
     path: 'help',
     loadComponent: () => import('./help-and-support/help-and-support')
       .then(m => m.HelpAndSupport),
-    title: 'Help and Support'
+    title: 'Help and Support',
+    canActivate: [authGuard]
   }
 ];
