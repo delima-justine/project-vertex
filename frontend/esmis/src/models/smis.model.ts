276272ffea8dbfe40864622e60b01d6fef042220
delima-justine <delimajustine24@gmail.com>
2026-04-12 13:22:52 +0800

    feat: add User and Role models for SMIS

--- frontend/esmis/src/models/smis.model.ts
+++ frontend/esmis/src/models/smis.model.ts
@@ -1,1 +1,15 @@
-// TODO: Add Models for SMIS
+export type Role = {
+  id: number;
+  role_name: string;
+};
+
+export type User = {
+  id: number;
+  first_name: string;
+  middle_initial?: string;
+  last_name: string;
+  email: string;
+  role_id: number;
+  office_id: number;
+  role?: Role;
+};
