6ca855165811160d0b09896108607740fa0be326
delima-justine <delimajustine24@gmail.com>
2026-04-12 13:23:04 +0800

    feat: load user role in user retrieval endpoint

--- backend/smis/routes/api.php
+++ backend/smis/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->load('role');
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
