a2a59cc1b8905283f8f32a1a50d7ac5ade040b38
delima-justine <delimajustine24@gmail.com>
2026-04-12 13:23:24 +0800

    feat: update AuthService to use User type and fetch user data on initialization

--- frontend/esmis/src/services/auth.service.ts
+++ frontend/esmis/src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { tap } from 'rxjs';
+import { User } from '../models/smis.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,29 @@ export class AuthService {
   private apiUrl = 'http://localhost:8000/api';
 
   // Use a signal to track auth state
-  currentUser = signal<any | null>(null);
+  currentUser = signal<User | null>(null);
+
+  constructor() {
+    // If we have a token, fetch the user data on initialization
+    if (this.isLoggedIn()) {
+      this.getUser().subscribe();
+    }
+  }
 
   login(credentials: any) {
     return this.http.post(`${this.apiUrl}/login`, credentials).pipe(
       tap((response: any) => {
         localStorage.setItem('auth_token', response.token);
-        this.currentUser.set(response.user);
+        this.getUser().subscribe();
+      })
+    );
+  }
+
+  // New method to fetch user profile with role
+  getUser() {
+    return this.http.get<User>(`${this.apiUrl}/user`).pipe(
+      tap((user: User) => {
+        this.currentUser.set(user);
       })
     );
   }
