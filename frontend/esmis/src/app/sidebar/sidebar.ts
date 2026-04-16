77ea1af9bafcd1be5a6d906248a11e26c291375a
delima-justine <delimajustine24@gmail.com>
2026-04-17 00:08:24 +0800

    feat: inject AuthService into Sidebar component

--- frontend/esmis/src/app/sidebar/sidebar.ts
+++ frontend/esmis/src/app/sidebar/sidebar.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,6 +9,7 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  authService = inject(AuthService);
   isDropdownOpen = signal(localStorage.getItem('isDropdownOpen') === 'true');
 
   toggleDropdown() {
