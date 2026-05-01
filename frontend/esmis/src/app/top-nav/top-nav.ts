faef6969c4c9355649351ed42733e549a2d991c6
delima-justine <delimajustine24@gmail.com>
2026-05-01 16:31:13 +0800

    feat: implement logout module.

--- frontend/esmis/src/app/top-nav/top-nav.ts
+++ frontend/esmis/src/app/top-nav/top-nav.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
@@ -8,12 +10,25 @@ import { Component, signal } from '@angular/core';
 })
 export class TopNav {
   isOpen = signal(false);
+  authService = inject(AuthService);
+  user = this.authService.currentUser;
+  router = inject(Router);
+
   
   toggleDropdown() {
     this.isOpen.set(!this.isOpen());
   }
 
   logout() {
-    console.log('User logged out');
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        localStorage.removeItem('auth_token');
+        this.authService.currentUser.set(null);
+        this.router.navigate(['/']);
+      }
+    });
   }
 }
