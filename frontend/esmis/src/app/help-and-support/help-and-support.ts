d486fb2a70c462fa8f98a803838b08e8760138e5
delima-justine <delimajustine24@gmail.com>
2026-05-01 23:06:33 +0800

    feat: integrate TopNav component into Help and Support page layout

--- frontend/esmis/src/app/help-and-support/help-and-support.ts
+++ frontend/esmis/src/app/help-and-support/help-and-support.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
+import { TopNav } from "../top-nav/top-nav";
 
 @Component({
   selector: 'app-help-and-support',
-  imports: [Sidebar],
+  imports: [Sidebar, TopNav],
   templateUrl: './help-and-support.html',
   styleUrl: './help-and-support.scss',
 })
