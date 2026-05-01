117c874bd0a2317137dc0e28cf6c64ee34bec568
delima-justine <delimajustine24@gmail.com>
2026-05-01 23:19:33 +0800

    feat: integrate TopNav component into Reports page layout and refactor styles

--- frontend/esmis/src/app/reports/reports.ts
+++ frontend/esmis/src/app/reports/reports.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../sidebar/sidebar";
+import { TopNav } from "../top-nav/top-nav";
 
 @Component({
   selector: 'app-reports',
-  imports: [Sidebar],
+  imports: [Sidebar, TopNav],
   templateUrl: './reports.html',
   styleUrl: './reports.scss',
 })
