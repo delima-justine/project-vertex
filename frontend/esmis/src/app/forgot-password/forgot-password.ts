e1f1fa490146385340b6e023cd4d435fdfebca9b
delima-justine <delimajustine24@gmail.com>
2026-05-02 22:37:49 +0800

    feat: implement forgot password feature with form validation and styling

--- frontend/esmis/src/app/forgot-password/forgot-password.ts
+++ frontend/esmis/src/app/forgot-password/forgot-password.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
 export class ForgotPassword {
+  forgotPasswordForm: FormGroup;
+  formBuilder = inject(FormBuilder);
 
+  constructor() {
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  sendResetLink() {
+    console.log('Reset link sent to:', this.forgotPasswordForm.value.email);
+  }
+
+  get email() {
+    return this.forgotPasswordForm.get('email');
+  }
 }
