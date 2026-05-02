36322eed54967dce97b24aaddf0b752a914ba1bd
Justine <delimajustine24@gmail.com>
2026-05-02 18:55:43 +0800

    Revert "feat: add ng2-charts provider to app configuration for charting capabilities"
    This reverts commit ea60d04f036db05fca72bd9a8c950820e1f5a994.

--- frontend/esmis/src/app/app.config.ts
+++ frontend/esmis/src/app/app.config.ts
@@ -5,7 +5,6 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { authInterceptor } from './auth-interceptor';
 import { provideLoadingBarInterceptor } from '@ngx-loading-bar/http-client';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor]), withInterceptorsFromDi()),
-    provideLoadingBarInterceptor(),
-    provideCharts(withDefaultRegisterables())
+    provideLoadingBarInterceptor()
   ]
 };
