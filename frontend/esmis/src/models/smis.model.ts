7b6c8c1c77a234e3951b3f227b4647ead0cdc865
delima-justine <delimajustine24@gmail.com>
2026-05-03 22:26:16 +0800

    feat: add PaginatedResponse type for handling paginated API responses

--- frontend/esmis/src/models/smis.model.ts
+++ frontend/esmis/src/models/smis.model.ts
@@ -116,3 +116,19 @@ export type LoginCredentials = {
   email: string;
   password: string;
 };
+
+export type PaginatedResponse<T> = {
+  current_page: number;
+  data: T[];
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  links: { url: string | null; label: string; active: boolean }[];
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+};
