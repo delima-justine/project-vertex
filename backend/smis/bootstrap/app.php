d696ee8088411133a3a927b7309b041ead1d9131
delima-justine <delimajustine24@gmail.com>
2026-05-05 21:59:43 +0800

    feat: update CORS configuration to use a static frontend URL

--- backend/smis/bootstrap/app.php
+++ backend/smis/bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withBroadcasting(
         __DIR__.'/../routes/channels.php',
-        ['middleware' => ['api', 'auth:sanctum']],
+        ['prefix' => 'api', 'middleware' => ['api', 'auth:sanctum']],
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
