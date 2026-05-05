54b63daed4cfb75236c35fc44a0b1fc7f605cb3b
delima-justine <delimajustine24@gmail.com>
2026-05-05 22:00:13 +0800

    feat: update apiUrl in environment configuration to include '/api' path

--- frontend/esmis/src/environments/environment.ts
+++ frontend/esmis/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'http://localhost:8000/api',
   pusherKey: 'f4968c707de726253286',
   pusherCluster: 'ap1',
   forceTLS: false
