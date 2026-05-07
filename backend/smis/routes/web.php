ee94f011b0cfe811ea0549755b60658da710a3f8
delima-justine <delimajustine24@gmail.com>
2026-05-08 07:43:26 +0800

    refactor: remove unused route definitions and controllers from web.php

--- backend/smis/routes/web.php
+++ backend/smis/routes/web.php
@@ -1,28 +1,7 @@
 <?php
 
-use App\Http\Controllers\ArchiveController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\OfficeController;
-use App\Http\Controllers\RolesController;
-use App\Http\Controllers\SupplyController;
-use App\Http\Controllers\UnitController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SupplyRequestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Routes for tbl_user
-Route::resource('users', UserController::class);
-Route::get('/users', [UserController::class, 'index']);
-Route::resource('supplies', SupplyController::class);
-Route::resource('units', UnitController::class);
-Route::resource('roles', RolesController::class);
-Route::resource('supply-requests', SupplyRequestController::class);
-Route::resource('offices', OfficeController::class);
-Route::apiResource('notifications', NotificationController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('archives', ArchiveController::class);
