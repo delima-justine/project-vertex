126adeb7378663efde1fa01094e83e7c377ebbcf
delima-justine <delimajustine24@gmail.com>
2026-05-10 01:44:21 +0800

    fix: replace auth guard with permission guard for route access control

--- frontend/esmis/src/app/app.routes.ts
+++ frontend/esmis/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { guestGuard } from './guards/guest-guard';
 import { authGuard } from './guards/auth-guard';
+import { permissionGuard } from './guards/permission-guard';
 
 export const routes: Routes = [
   {
@@ -21,8 +22,8 @@ export const routes: Routes = [
     loadComponent: () => import('./user-management/user-management')
       .then(m => m.UserManagement),
     title: 'User Management',
-    canActivate: [authGuard]
-
+    canActivate: [permissionGuard],
+    data: { permissions: ['add_user', 'edit_user', 'delete_user'] }
   },
   {
     path: 'notifications',
@@ -48,26 +49,30 @@ export const routes: Routes = [
     path: 'pending-requests',
     loadComponent: () => import('./pending/pending').then(m => m.Pending),
     title: 'Pending Requests',
-    canActivate: [authGuard]
+    canActivate: [permissionGuard],
+    data: { permissions: ['view_pending_requests'] }
   },
   {
     path: 'approved-requests',
     loadComponent: () => import('./approved/approved').then(m => m.Approved),
     title: 'Approved Requests',
-    canActivate: [authGuard]
+    canActivate: [permissionGuard],
+    data: { permissions: ['view_approved_requests'] }
   },
   {
     path: 'released-requests',
     loadComponent: () => import('./released/released').then(m => m.Released),
     title: 'Released Requests',
-    canActivate: [authGuard]
+    canActivate: [permissionGuard],
+    data: { permissions: ['view_released_requests'] }
   },
   {
     path: 'disapproved-requests',
     loadComponent: () => import('./disapproved/disapproved')
       .then(m => m.Disapproved),
     title: 'Disapproved Requests',
-    canActivate: [authGuard]
+    canActivate: [permissionGuard],
+    data: { permissions: ['view_disapproved_requests'] }
   },
   {
     path: 'help',
@@ -81,7 +86,8 @@ export const routes: Routes = [
     loadComponent: () => import('./edit-ris/edit-ris')
       .then(m => m.EditRis),
     title: 'Edit RIS',
-    canActivate: [authGuard]
+    canActivate: [permissionGuard],
+    data: { permissions: ['edit_ris'] }
   },
   {
     path: 'forgot-pass',
