2c35e6cd80d180614821475bed08a31ebaf9ab58
delima-justine <delimajustine24@gmail.com>
2026-05-13 03:27:16 +0800

    feat: add strong password validator to enhance password security

---
+++ frontend/esmis/src/validators/password.validator.ts
@@ -0,0 +1,31 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function strongPasswordValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumber = /[0-9]/.test(value);
+    const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(value);
+
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumber && hasSpecialCharacter;
+
+    if (!passwordValid) {
+      return {
+        strongPassword: {
+          hasUpperCase,
+          hasLowerCase,
+          hasNumber,
+          hasSpecialCharacter
+        }
+      };
+    }
+
+    return null;
+  };
+}
