be0e8ab38b4d200d997b0cfd0062ca1c531bc5dc
delima-justine <delimajustine24@gmail.com>
2026-05-23 13:44:16 +0800

    refactor: add identities configuration to environment files

--- frontend/esmis/src/environments/environment.prod.ts
+++ frontend/esmis/src/environments/environment.prod.ts
@@ -3,5 +3,21 @@ export const environment = {
   apiUrl: 'https://astral3.ojt-ims-bsit.net/api',
   pusherKey: 'f4968c707de726253286',
   pusherCluster: 'ap1',
-  forceTLS: true
+  forceTLS: true,
+  identities: {
+    republicName: 'Republic of the Philippines',
+    orgName: 'The Name of the University',
+    orgBranch: 'This Campus',
+    officeName: 'This Office of the Campuses',
+    directorName: 'Jane Dee',
+    directorTitle: 'Director',
+    custodianName: 'John Doe',
+    custodianTitle: 'Administrator Office',
+    contactEmail: 'vertex.group.0311@gmail.com',
+    contactAddress: 'UNI, Administrator Office, Bldg B',
+    formCode: 'UNI-RISL-6-PSMO-010',
+    logoPath: 'assets/esmis_logo_l.png',
+    appLogoPath: 'assets/esmis_logo_l.png',
+    sidebarLogoPath: 'assets/esmis_logo.png'
+  }
 };
