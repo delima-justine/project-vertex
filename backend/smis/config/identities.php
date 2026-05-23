52de6565eaaa9f75aebc6492c426062536d0eca8
delima-justine <delimajustine24@gmail.com>
2026-05-23 13:41:08 +0800

    refactor: add identities configuration file for organizational and system details

---
+++ backend/smis/config/identities.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'org_name' => env('ORG_NAME', 'Polytechnic University of the Philippines'),
+    'org_acronym' => env('ORG_ACRONYM', 'PUP'),
+    'org_branch' => env('ORG_BRANCH', 'Taguig Branch'),
+    'system_name' => env('SYSTEM_NAME', 'Supply Management Information System'),
+    'system_acronym' => env('SYSTEM_ACRONYM', 'SMIS'),
+];
