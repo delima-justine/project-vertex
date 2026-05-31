d4f7049438c3d588ad913a7d7cc96cb78b0c668f
delima-justine <delimajustine24@gmail.com>
2026-05-31 14:48:13 +0800

    fix: update organization branch name to reflect correct campus

--- backend/smis/config/identities.php
+++ backend/smis/config/identities.php
@@ -3,7 +3,7 @@
 return [
     'org_name' => env('ORG_NAME', 'Polytechnic University of the Philippines'),
     'org_acronym' => env('ORG_ACRONYM', 'PUP'),
-    'org_branch' => env('ORG_BRANCH', 'Taguig Branch'),
+    'org_branch' => env('ORG_BRANCH', 'Taguig Campus'),
     'system_name' => env('SYSTEM_NAME', 'Supply Management Information System'),
     'system_acronym' => env('SYSTEM_ACRONYM', 'SMIS'),
 ];
